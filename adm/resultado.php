06fecccf21d2e38bf3642be96308fb3600cba9f9
rudanlucena <rudanlucena@hotmail.com>
2017-02-18 16:22:52 -0200

    bug de visualização resolvido

--- adm/resultado.php
+++ adm/resultado.php
@@ -3,14 +3,14 @@
     <?php  
           //esse bloco de código em php verifica se existe a sessão, pois o usuário pode simplesmente não fazer o login e digitar na barra de endereço do seu navegador o caminho para a página principal do site (sistema), burlando assim a obrigação de fazer um login, com isso se ele não estiver feito o login não será criado a session, então ao verificar que a session não existe a página redireciona o mesmo para a index.php.
           session_start();
-          if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+          if((!isset ($_SESSION['login_adm']) == true) and (!isset ($_SESSION['senha_adm']) == true))
           {
-            unset($_SESSION['login']);
-            unset($_SESSION['senha']);
+            unset($_SESSION['login_adm']);
+            unset($_SESSION['senha_adm']);
             header('location:index.php');
           }
 
-          $logado = $_SESSION['login'];
+          $logado = $_SESSION['login_adm'];
 
           /* dados passados pelo metodo get */
           $id_pergunta =$_GET['id_pergunta'];
